ebbc3c10dc755a44049618d733e4ebc66e638215
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-07-16 17:58:15 -0700

    fill in methods

--- database.py
+++ database.py
@@ -61,27 +61,66 @@ class DatabaseInterface:
         self.__conn.cursor().execute(table_creation_rule)
         self.__conn.commit()
 
+    def __set_column_value(self, server_id: str, col: Column, val: str) -> None:
+        query = f"""
+            INSERT INTO {self.__table_name} ({Column.id}, {col})
+            VALUES (?, ?)
+            ON CONFLICT({Column.id}) DO UPDATE SET
+                {col} = excluded.{col}
+        """  # noqa: S608
+        self.__conn.cursor().execute(query, (server_id, val))
+        self.__conn.commit()
+
+    def __get_column_value(self, server_id: str, col: Column) -> str | None:
+        query = f"SELECT {col} from {self.__table_name} where {Column.id} = ?"  # noqa: S608
+        cursor = self.__conn.cursor()
+        cursor.execute(query, (server_id,))
+
+        result = cursor.fetchone()
+        return result[0] if result else None
+
     def set_x_api_key(self, server_id: str, x_api_key: str) -> None:
-        raise NotImplementedError
+        self.__set_column_value(server_id, Column.x_api_key, x_api_key)
 
     def set_trigger_emoji(self, server_id: str, trigger_emoji: str) -> None:
-        raise NotImplementedError
+        self.__set_column_value(server_id, Column.trigger_emoji, trigger_emoji)
 
     def set_emoji_reaction_threshhold(self, server_id: str, threshhold: int) -> None:
-        raise NotImplementedError
+        self.__set_column_value(
+            server_id,
+            Column.emoji_reaction_threshhold,
+            str(threshhold),
+        )
 
     def set_reaction_observing_timeout_secs(
         self,
         server_id: str,
         timeout_secs: int,
     ) -> None:
-        raise NotImplementedError
+        self.__set_column_value(
+            server_id,
+            Column.reaction_observing_timeout_secs,
+            str(timeout_secs),
+        )
 
     def get_trigger_emoji(self, server_id: str) -> str | None:
-        raise NotImplementedError
+        return self.__get_column_value(server_id, Column.trigger_emoji)
 
     def get_emoji_reaction_threshhold(self, server_id: str) -> int | None:
-        raise NotImplementedError
+        maybe_threshhold = self.__get_column_value(
+            server_id,
+            Column.emoji_reaction_threshhold,
+        )
+        return int(maybe_threshhold) if maybe_threshhold else None
 
     def get_reaction_observing_timeout_secs(self, server_id: str) -> int:
-        raise NotImplementedError
+        maybe_timeout = self.__get_column_value(
+            server_id,
+            Column.reaction_observing_timeout_secs,
+        )
+
+        if maybe_timeout is None:
+            e = f"Expected non-null value for column '{Column.reaction_observing_timeout_secs}'"
+            raise ValueError(e)
+
+        return int(maybe_timeout)
