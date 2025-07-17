bb80011996bfba4443f823480c4536f7cd5037b0
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-07-16 17:23:39 -0700

    add column schema with automatic table creation in the interface constructor

--- database.py
+++ database.py
@@ -3,10 +3,22 @@ from __future__ import annotations
 import atexit
 import logging
 import sqlite3
+from enum import Enum
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pathlib import Path
+
+
+class Column(str, Enum):
+    id = "id"
+    x_api_key = "x_api_key"
+    trigger_emoji = "trigger_emoji"
+    emoji_reaction_threshhold = "emoji_reaction_threshhold"
+    reaction_observing_timeout_secs = "reaction_observing_timeout_secs"
+
+    def __str__(self) -> str:
+        return self.value
 
 
 class DatabaseInterface:
@@ -15,6 +27,9 @@ class DatabaseInterface:
 
         self.__logger = logging.getLogger(__name__)
         self.__conn = self.__db_conn_init(database_path)
+        self.__table_name = "server_configs"
+
+        self.__create_table_if_needed()
 
         atexit.register(self.__close)
 
@@ -32,6 +47,19 @@ class DatabaseInterface:
             )
 
         return sqlite3.connect(self.db_path)
+
+    def __create_table_if_needed(self) -> None:
+        table_creation_rule = f"""
+        CREATE TABLE IF NOT EXISTS {self.__table_name} (
+            {Column.id} TEXT PRIMARY KEY,
+            {Column.x_api_key} TEXT,
+            {Column.trigger_emoji} TEXT,
+            {Column.emoji_reaction_threshhold} INTEGER,
+            {Column.reaction_observing_timeout_secs} INTEGER DEFAULT {60 * 5}
+        )
+        """
+        self.__conn.cursor().execute(table_creation_rule)
+        self.__conn.commit()
 
     def set_x_api_key(self, server_id: str, x_api_key: str) -> None:
         raise NotImplementedError
