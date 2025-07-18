e2b8214fe8971b8b73610a05077fe00bd4c78af6
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-07-18 15:14:58 -0700

    refactor common post logic and auth

--- x.py
+++ x.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
+from tweepy import API, OAuth1UserHandler
 from tweepy.asynchronous import AsyncClient
 
 type URL = str
@@ -16,6 +19,7 @@ class AuthenticationBundle:
 
 class Account:
     def __init__(self, auth: AuthenticationBundle) -> None:
+        self.auth = auth
         self.client = AsyncClient(
             bearer_token=auth.bearer_token,
             consumer_key=auth.api_key,
@@ -24,17 +28,32 @@ class Account:
             access_token_secret=auth.access_token_secret,
         )
 
-    async def get_username(self) -> str:
+    async def __post(
+        self,
+        *,
+        text: str | None = None,
+        media_ids: str | None = None,
+    ) -> URL:
+        response = await self.client.create_tweet(text=text, media_ids=media_ids)
+        post_id = response.data["id"]
+
+        return await self.__post_url_from(post_id)
+
+    async def __get_username(self) -> str:
         return str((await self.client.get_me()).data.username)
 
-    async def post_url_from(self, post_id: str) -> URL:
-        username = await self.get_username()
+    def __get_oauth_user_handler(self) -> OAuth1UserHandler:
+        return OAuth1UserHandler(
+            self.auth.api_key,
+            self.auth.api_key_secret,
+            self.auth.access_token,
+            self.auth.access_token_secret,
+        )
+
+    async def __post_url_from(self, post_id: str) -> URL:
+        username = await self.__get_username()
 
         return f"https://x.com/{username}/status/{post_id}"
 
     async def create_text_post(self, text: str) -> URL:
-        response = await self.client.create_tweet(text=text)
-
-        post_id = response.data["id"]
-
-        return await self.post_url_from(post_id)
+        return await self.__post(text=text)
